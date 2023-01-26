43479dc2a5a643ce876e50d093b739648ae64a38
Владимир <vladimir>
2023-01-26 06:23:28 +0300

    for front

--- back/main.go
+++ back/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"os"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	data1, err := os.ReadFile("htmlfiles/htmlExample1.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data2, err := os.ReadFile("htmlfiles/htmlExample2.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data3, err := os.ReadFile("htmlfiles/htmlExample3.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := bytes.Join(
+		[][]byte{
+			data1, data2, data3,
+		},
+		[]byte{},
+	)
+	_, err = w.Write(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":3000", nil)
 }
